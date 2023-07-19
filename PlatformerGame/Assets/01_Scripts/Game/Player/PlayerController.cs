931693a161870fdf539e24642eb393632a52621b
Jaemin <30969773+IUCyH@users.noreply.github.com>
2023-07-19 16:49:27 +0900

    점프 버그는 키보드 고장으로 인한 것으로 수정 불필요

--- PlatformerGame/Assets/01_Scripts/Game/Player/PlayerController.cs
+++ PlatformerGame/Assets/01_Scripts/Game/Player/PlayerController.cs
@@ -120,7 +120,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
         if (!stopMovement)
         {
             var dir = InputManager.GetAxisRaw(Axis.Horizontal);
