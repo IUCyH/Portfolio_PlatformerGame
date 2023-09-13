3892f6dd65083c2fb7a413b464b275694491f98a
Jaemin <30969773+IUCyH@users.noreply.github.com>
2023-09-13 16:08:08 +0900

    PopupManager에서 PopupPool 생성 로직 작성중
    TODO : for문안에서 각 생성된 IPopup 객체를 for문 밖에서 반환할 수 있도록 수정

--- PlatformerGame/Assets/01_Scripts/UI/Popup/PopupManager.cs
+++ PlatformerGame/Assets/01_Scripts/UI/Popup/PopupManager.cs
@@ -15,6 +15,23 @@ public class PopupManager : Singleton_DontDestroy<PopupManager>
 
     protected override void OnStart()
     {
+        var popups = Resources.LoadAll<GameObject>("Prefabs");
+        var generateCount = popups.Length * 2;
         
+        /*popupPool = new ObjectPool<IPopup>(generateCount, () =>
+        {
+            var popupPrefabs = popups;
+
+            for (int i = 0; i < popupPrefabs.Length; i++)
+            {
+                var obj = Instantiate(popupPrefabs[i]);
+                obj.transform.position = Vector3.zero;
+                obj.transform.localScale = Vector3.one;
+                
+                obj.SetActive(false);
+            }
+            
+            
+        });*/
     }
 }
